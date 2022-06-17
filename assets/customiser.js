ValerioLyndon/Theme-Customiser

commit 3c07fa2a9414dbf2b9af2e2476e500b72b3b1ff5

Support mods with external links
For when mods need a full guide or are not supported by the customiser, you can link to an external resource..

assets/customiser.js
@@ -727,24 +727,41 @@ function renderHtml() {
 		for (const [modId, mod] of Object.entries(theme['mods'])) {
 
 			let div = document.createElement('div'),
-				head = document.createElement('b'),
+				head = document.createElement('div'),
+				headLeft = document.createElement('b'),
+				headRight = document.createElement('div');
 				expando = document.createElement('div'),
-				desc = document.createElement('div'),
-				toggle = document.createElement('div');
+				desc = document.createElement('div');
 
-			toggle.className = 'entry__toggle-box';
-			toggle.innerHTML = `
-				<input id="theme-${modId}" type="checkbox" class="o-hidden" />
-				<label class="toggle" for="theme-${modId}">
-					<div class="toggle__info"></div>
-				</label>
-			`;
-			div.appendChild(toggle);
+			if('css' in mod) {
+				headRight.innerHTML = `
+					<input id="theme-${modId}" type="checkbox" class="o-hidden" />
+					<label class="toggle info-popup" for="theme-${modId}">
+						<div class="info-popup__box"></div>
+					</label>
+				`;
+			} else if('url' in mod) {
+				let link = document.createElement('a');
+				link.className = 'entry__external-link info-popup info-popup--activated-on-hover';
+				link.href = mod['url'];
+				link.target = "_blank";
+				link.innerHTML = `
+					<i class="entry__external-link-icon fa-solid fa-arrow-up-right-from-square"></i>
+					<div class="info-popup__box">
+						This mod has linked an external resource or guide for you to install. Unless otherwise instructed, these should be installed <b>after</b> you install the main theme.
+					</div>
+				`;
+				headRight.appendChild(link);
+			}
 
 			div.className = 'entry';
 			div.id = `js-theme-${modId}-parent`;
-			head.textContent = mod['name'];
-			head.className = 'entry__name entry__name--emphasised';
+			head.className = 'entry__head';
+			headLeft.textContent = mod['name'];
+			headLeft.className = 'entry__name entry__name--emphasised';
+			headRight.className = 'entry__action-box';
+			head.appendChild(headLeft);
+			head.appendChild(headRight);
 			div.appendChild(head);
 
 			expando.className = 'expando js-expando';
@@ -776,7 +793,9 @@ function renderHtml() {
 
 			modsEle.appendChild(div);
 
-			document.getElementById(`theme-${modId}`).addEventListener('change', () => { updateMod(modId); });
+			if('css' in mod) {
+				document.getElementById(`theme-${modId}`).addEventListener('change', () => { updateMod(modId); });
+			}
 
 			// Add mod tag to list of tags
 			if('tags' in mod) {
@@ -993,7 +1012,8 @@ function renderHtml() {
 				{ height: `${expandedHeight}px` },
 				{ height: `${collapsedHeight}px`}
 			];
-			parent.style = `height: ${collapsedHeight}px`;
+			parent.style.height = `${collapsedHeight}px`;
+			parent.style.paddingBottom = `0px`;
 			parent.classList.remove('is-expanded');
 			parent.animate(animFrames, animTiming);
 			this.textContent = 'Expand';
@@ -1003,7 +1023,8 @@ function renderHtml() {
 				{ height: `${expandedHeight + 25}px`,
 				  paddingBottom: '25px' }
 			];
-			parent.style = `height: auto; padding-bottom: 25px;`;
+			parent.style.height = `auto`;
+			parent.style.paddingBottom = `25px`;
 			parent.classList.add('is-expanded');
 			parent.animate(animFrames, animTiming);
 			this.textContent = 'Collapse';

assets/customiser.css
@@ -117,7 +117,6 @@ body {
 /* Checkbox toggles */
 
 .toggle {
-	position: relative;
 	display: block;
 	width: 32px;
 	height: 16px;
@@ -158,9 +157,13 @@ input:checked + .toggle.is-forced::after {
 .toggle.has-info {
 	cursor: help;
 }
-.toggle__info {
+
+.info-popup {
+	position: relative;
+}
+.info-popup__box {
 	position: absolute;
-	bottom: 22px;
+	bottom: calc(100% + 10px);
 	right: -8px;
 	z-index: 5;
 	display: none;
@@ -169,33 +172,36 @@ input:checked + .toggle.is-forced::after {
 	background: var(--background);
 	border: 2px solid var(--border-light);
 
+	color: var(--text);
+
 	box-shadow: 2px 3px 6px rgba(0,0,0,0.3);
 	opacity: 0;
 	transition: opacity 0.1s ease;
 	pointer-events: none;
 }
-.toggle:hover .toggle__info {
+.info-popup:hover .info-popup__box {
 	opacity: 1;
 }
-.toggle.has-info .toggle__info {
+.info-popup--activated-on-hover .info-popup__box,
+.info-popup.has-info .info-popup__box {
 	display: block;
 }
 
 /* decorative arrow */
-.toggle__info::before,
-.toggle__info::after {
+.info-popup__box::before,
+.info-popup__box::after {
 	content: "";
 	position: absolute;
 	border-style: solid;
 	border-color: transparent;
 }
-.toggle__info::before {
+.info-popup__box::before {
 	top: 100%;
 	right: 12px;
 	border-width: 10px;
 	border-top-color: var(--border-light);
 }
-.toggle__info::after {
+.info-popup__box::after {
 	bottom: -12px;
 	right: 16px;
 	border-width: 6px;
@@ -206,10 +212,11 @@ input:checked + .toggle.is-forced::after {
 /* Expando */
 
 .expando {
+	--margin: 5px;
 	position: relative;
-	width: calc(100% + 10px);
-	padding: 0 5px;
-	left: -5px;
+	width: calc(100% + var(--margin) * 2);
+	padding: 0 var(--margin);
+	left: calc(var(--margin) * -1);
 	overflow: hidden;
 }
 .expando::after {
@@ -384,8 +391,14 @@ input:checked + .toggle.is-forced::after {
 	display: none;
 }
 
+.entry__head {
+	display: flex;
+	height: 22px;
+}
 .entry__name {
 	display: block;
+	width: 50%;
+	flex: 2 0 auto;
 	font-weight: 400;
 	margin-bottom: 2px;
 }
@@ -425,8 +438,14 @@ input:checked + .toggle.is-forced::after {
 	border-radius: 50%;
 }
 
-.entry__toggle-box {
-	float: right;
+.entry__external-link {
+	color: var(--text);
+}
+.entry__external-link:hover {
+	color: var(--accent);
+}
+.entry__external-link-icon {
+	font-size: 20px;
 }
 
 .entry__select {

index.html
@@ -40,7 +40,7 @@
 				</div>
 
 				<div class="sidebar__section">
-					<div class="expando js-expando" data-expando-limit="400" id="js-options">
+					<div class="expando js-expando" data-expando-limit="400" style="--margin: 16px" id="js-options">
 						<button class="expando__button js-expando-button">Expand</button>
 						
 						<h3 class="sidebar__header">Change theme options</h3>
@@ -50,7 +50,7 @@
 				</div>
 
 				<div class="sidebar__section">
-					<div class="expando js-expando" data-expando-limit="400" id="js-mods">
+					<div class="expando js-expando" data-expando-limit="400" style="--margin: 16px" id="js-mods">
 						<button class="expando__button js-expando-button">Expand</button>
 						
 						<h3 class="sidebar__header">

commit d15187297b1deb19555d7fb83699b2f3aaac98a2

Fix option CSS.
Regression from 3c07fa2a9414dbf2b9af2e2476e500b72b3b1ff5

assets/customiser.js
@@ -566,7 +566,9 @@ function renderHtml() {
 			opt = dictionary[1];
 
 		let div = document.createElement('div'),
-			head = document.createElement('b'),
+			head = document.createElement('div'),
+			headLeft = document.createElement('b'),
+			headRight = document.createElement('div');
 			expando = document.createElement('div'),
 			desc = document.createElement('div'),
 			notice = document.createElement('p'),
@@ -579,8 +581,12 @@ function renderHtml() {
 		}
 
 		div.className = 'entry has-help';
-		head.textContent = opt['name'];
-		head.className = 'entry__name';
+		head.className = 'entry__head';
+		headLeft.textContent = opt['name'];
+		headLeft.className = 'entry__name';
+		headRight.className = 'entry__action-box';
+		head.appendChild(headLeft);
+		head.appendChild(headRight);
 		div.appendChild(head);
 
 		expando.className = 'expando js-expando';
@@ -670,16 +676,21 @@ function renderHtml() {
 		}
 
 		else if(baseType === 'toggle') {
-			let toggle = document.createElement('div');
-
-			toggle.className = 'entry__toggle-box';
-			toggle.innerHTML = `
-				<input id="theme-${id}" type="checkbox" class="o-hidden" ${('default' in opt && opt['default'] == true) ? 'checked="checked"' : ''}" />
-				<label class="toggle" for="theme-${id}">
-					<div class="toggle__info"></div>
+			let toggle = document.createElement('input');
+			toggle.type = 'checkbox';
+			toggle.id = `theme-${id}`;
+			toggle.className = 'o-hidden';
+			if('default' in opt && opt['default'] == true) {
+				toggle.checked = true;
+			}
+			headRight.innerHTML = `
+				<label class="toggle info-popup" for="theme-${id}">
+					<div class="info-popup__box"></div>
 				</label>
 			`;
-			div.prepend(toggle);
+			headRight.prepend(toggle);
+
+
 
 			toggle.addEventListener('input', () => { updateOption(id, opt['default'], parentModId); });
 		}
@@ -1081,7 +1092,8 @@ function importPreviousOpts(opts = undefined) {
 
 		try {
 			var previousOpts = JSON.parse(previous);
-		} catch {
+		} catch(e) {
+			console.log(`[importPreviousOpts] Error during JSON.parse: ${e}`);
 			messenger.error('Import failed, could not interpret your options. Are you sure you copied and pasted all the settings?', 'json.parse');
 			return false;
 		}
@@ -1149,7 +1161,8 @@ function finalSetup() {
 			} else {
 				try {
 					opts = JSON.parse(opts);
-				} catch {
+				} catch(e) {
+					console.log(`[finalise] Error during JSON.parse: ${e}`);
 					messenger.error('Failed to import options. Could not parse settings.', 'json.stringify');
 				}
 				// importpreviousopts will call updateCss and pushCss
@@ -1206,6 +1219,7 @@ fetchData.then((json) => {
 	try {
 		data = JSON.parse(json);
 	} catch(e) {
+		console.log(`[fetchData] Error during JSON.parse: ${e}`);
 		loader.failed(['Encountered a problem while parsing theme information.', 'json.parse']);
 	}
 