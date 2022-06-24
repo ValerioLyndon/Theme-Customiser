ValerioLyndon/Theme-Customiser

commit 37c281ade4da879c36545edda482dc414ffcb1ae

Prevent options from affecting settings string
Now inserts settings to the top of the code after all other processing has been done to prevent options (or mods) from affecting it.

assets/customiser.js
@@ -290,14 +290,7 @@ function updateMod(id) {
 
 function updateCss() {
 	let newCss = baseCss;
-
-	// Encode options & sanitise any CSS character
-	let optsStr = JSON.stringify(userOpts).replaceAll('*/','*\\/').replaceAll('/*','\\/*');
-	// Place options into export area
-	document.getElementById('js-export-code').textContent = optsStr;
-	// Place options at top
-	newCss = '/* Theme Customiser Settings\nhttps://github.com/ValerioLyndon/Theme-Customiser\n^TC' + optsStr + 'TC$*/\n\n' + baseCss;
-
+	
 	function applyOptionToCss(css, optData, insert) {
 		let qualifier = optData['type'].split('/')[1],
 			subQualifier = optData['type'].split('/')[2];
@@ -403,7 +396,14 @@ function updateCss() {
 	}
 
 	function pushCss(css) {
-		// Update output
+		// Encode options & sanitise any CSS character
+		let optsStr = JSON.stringify(userOpts).replaceAll('*/','*\\/').replaceAll('/*','\\/*');
+		// Update export textareas
+		document.getElementById('js-export-code').textContent = optsStr;
+		// Place options at top
+		newCss = '/* Theme Customiser Settings\nhttps://github.com/ValerioLyndon/Theme-Customiser\n^TC' + optsStr + 'TC$*/\n\n' + baseCss;
+
+		// Update code textarea
 		document.getElementById('js-output').textContent = css;
 
 		// Add notice if necessary

commit 1e2837d6119d246c0bebcbc89f41b4f729bf3948

Fix preset missing from output
Regression from 37c281ade4da879c36545edda482dc414ffcb1ae

assets/customiser.js
@@ -409,7 +409,7 @@ function updateCss() {
 		// Update export textareas
 		document.getElementById('js-export-code').textContent = optsStr;
 		// Place options at top
-		newCss = '/* Theme Customiser Settings\nhttps://github.com/ValerioLyndon/Theme-Customiser\n^TC' + optsStr + 'TC$*/\n\n' + baseCss;
+		css = '/* Theme Customiser Settings\nhttps://github.com/ValerioLyndon/Theme-Customiser\n^TC' + optsStr + 'TC$*/\n\n' + css;
 
 		// Update code textarea
 		document.getElementById('js-output').textContent = css;

assets/receiver.js
@@ -21,7 +21,8 @@ function setColumns(columns) {
 window.addEventListener(
 	"message",
 	function (event) {
-		if (event.origin === window.location.origin) {
+		if(event.origin === window.location.origin) {
+			console.log('[receiver.js] Message received.');
 			var push = event.data || event.message,
 				type = push[0],
 				content = push[1];