ValerioLyndon/Theme-Customiser

commit a0e477304f35c09d0f6540462b6479f821141ce3

Add "category" to theme preview options
"preview" key in theme JSON now accepts "category" key with a number value. This will set the category to first display on page load.

assets/preview_modern.js
@@ -28,21 +28,26 @@ var categoryCodes = {
 	6: 'planned'
 }
 
-function changeCategory() {
-	let catId = this.href.substring(button.href.length - 1),
-		catClass = this.className.substring(14),
-		catName = categoryNames[catId],
-		listItems = document.getElementsByClassName('list-item');
+function changeCategory(catId, catBtn) {
+	if(!catBtn) {
+		for(let button of categoryButtons) {
+			if(button.href.includes(`status=${catId}`)) {
+				catBtn = button;
+			}
+		}
+	}
 	
 	// Change basic attributes
+	catBtn.classList.add('on');
 	document.body.setAttribute('data-query',`{"status":${catId}}`);
 	document.querySelector('.status-button.on').classList.remove('on');
-	this.classList.add('on');
-	document.getElementsByClassName('list-unit')[0].className = `list-unit ${catClass}`;
-	document.querySelector('.list-status-title .text').textContent = catName.toUpperCase();
+	document.getElementsByClassName('list-unit')[0].className = `list-unit ${catBtn.className.substring(14)}`;
+	document.querySelector('.list-status-title .text').textContent = categoryNames[catId].toUpperCase();
 
 	// Hide relevant list items
-	for(item of listItems) {
+	let listItems = document.getElementsByClassName('list-item');
+
+	for(let item of listItems) {
 		if(catId === '7' || item.getAttribute('tc-category') === categoryCodes[catId]) {
 			item.style = '';
 		} else {
@@ -50,8 +55,11 @@ function changeCategory() {
 		}
 	}
 }
-for(var button of categoryButtons) {
-	button.addEventListener('click', changeCategory.bind(button));
+for(let button of categoryButtons) {
+	button.addEventListener('click', () => {
+		let catId = button.href.substring(button.href.length - 1);
+		changeCategory(catId, button);
+	});
 }
 
 // Fixed header

assets/customiser.js
@@ -1043,24 +1043,32 @@ function renderHtml() {
 
 	// Set preview options
 
-	if(theme['type'] === 'classic') {
-		document.getElementById('js-preview-options__cover').remove();
-	}
-	else if('preview' in theme && 'cover' in theme['preview']) {
-		let check = document.getElementById('js-preview__cover'),
-			toggle = check.nextElementSibling,
-			val = true;
-
-		if(!theme['preview']['cover']) {
-			val = false;
-			toggle.classList.add('is-disabled', 'has-info');
-		} else {
-			toggle.classList.add('is-forced', 'has-info');
+	if('preview' in theme) {
+		// Cover
+		if(theme['type'] === 'classic') {
+			document.getElementById('js-preview-options__cover').remove();
 		}
-		check.checked = val;
-		check.disabled = true;
-		toggle.removeAttribute('onclick');
-		postToIframe(['cover', val]);
+		else if('cover' in theme['preview']) {
+			let check = document.getElementById('js-preview__cover'),
+				toggle = check.nextElementSibling,
+				val = true;
+
+			if(!theme['preview']['cover']) {
+				val = false;
+				toggle.classList.add('is-disabled', 'has-info');
+			} else {
+				toggle.classList.add('is-forced', 'has-info');
+			}
+			check.checked = val;
+			check.disabled = true;
+			toggle.removeAttribute('onclick');
+			postToIframe(['cover', val]);
+		}
+
+		// Category
+		if('category' in theme['preview']) {
+			postToIframe(['category', theme['preview']['category']])
+		}
 	}
 
 	// Set theme columns and push to iframe

assets/preview_classic.js
@@ -27,21 +27,25 @@ var categoryCodes = {
 	6: 'planned'
 }
 
-function changeCategory() {
-	let a = this.getElementsByTagName('a')[0],
-		indexStart = a.href.indexOf('status=') + 7,
-		indexEnd = indexStart + 1,
-		catId = a.href.substring(indexStart, indexEnd);
-	
-	// Change basic attributes
-	for(var button of categoryButtons) {
+function changeCategory(catId, catBtn) {
+	if(!catBtn) {
+		for(let button of categoryButtons) {
+			let href = button.getElementsByTagName('a')[0].href;
+			if(href.includes(`status=${catId}`)) {
+				catBtn = button;
+			}
+		}
+	}
+
+	// Change button attributes
+	for(let button of categoryButtons) {
 		button.className = 'status_not_selected';
 	}
-	this.className = 'status_selected';
+	catBtn.className = 'status_selected';
 
 	// Hide relevant tables
 	let tables = document.getElementsByTagName('table');
-	console.log(catId);
+
 	for(let table of tables) {
 		let tableCat = table.getAttribute('tc-category');
 		if(catId === '7' || tableCat === categoryCodes[catId]) {
@@ -53,150 +57,157 @@ function changeCategory() {
 		}
 	}
 }
-for(var button of categoryButtons) {
-	button.addEventListener('click', changeCategory.bind(button));
+
+for(let button of categoryButtons) {
+	button.addEventListener('click', () => {
+		let a = button.getElementsByTagName('a')[0],
+			indexStart = a.href.indexOf('status=') + 7,
+			indexEnd = indexStart + 1,
+			catId = a.href.substring(indexStart, indexEnd);
+		changeCategory(catId, button);
+	});
 }
 
 // Change list view
 
 let menuHtml = {
 	'owner': `<tbody><tr>
-                      <td id="mal_cs_listinfo">
-          <div>
-            <strong>
-              <a onclick="return false;" href="/profile/Example">
-                <strong>Example</strong>
-              </a>
-            </strong>
-          </div>
-          <div>
-            <form action="https://myanimelist.net/logout.php" method="post">
-  <a onclick="return false;" href="javascript:void(0);" onclick="$(this).parent().submit();" class="logout">
-    <i class="fa-solid fa-fw fa-right-from-bracket"></i>
-    Logout
-  </a>
+											<td id="mal_cs_listinfo">
+					<div>
+						<strong>
+							<a onclick="return false;" href="/profile/Example">
+								<strong>Example</strong>
+							</a>
+						</strong>
+					</div>
+					<div>
+						<form action="https://myanimelist.net/logout.php" method="post">
+	<a onclick="return false;" href="javascript:void(0);" onclick="$(this).parent().submit();" class="logout">
+		<i class="fa-solid fa-fw fa-right-from-bracket"></i>
+		Logout
+	</a>
 </form>
-          </div>
-        </td>
-        <td id="mal_cs_links">
-          <div>
-            <a onclick="return false;" href="/addtolist.php?hidenav=1" class="List_LightBox">Add to List</a>
-            <a onclick="return false;" href="/">Home</a>
-          </div>
-          <div>
-            <a onclick="return false;" href="/animelist/Example">Anime List</a>
-            <a onclick="return false;" href="/mangalist/Example">Manga List</a>
-          </div>
-        </td>
-        <td id="mal_cs_otherlinks">
-                      <div>
-              <strong>
-                You are viewing your anime list
-              </strong>
-            </div>
-            <div>
-              <a onclick="return false;" href="/history/Example">History</a>
-              <a onclick="return false;" href="/forum/">Forum</a>
-              <a onclick="return false;" href="/panel.php?go=export">Export</a>
-            </div>
-                  </td>
-            <td id="mal_cs_powered" valign="top" align="right">
-        <a onclick="return false;" href="/"><img class=" lazyloaded" data-src="https://cdn.myanimelist.net/images/list-top-powered.gif" src="https://cdn.myanimelist.net/images/list-top-powered.gif"></a>
-        <div id="search">
-          <input id="searchBox" value="Search" type="textbox">
-          <img class=" lazyloaded" data-src="https://cdn.myanimelist.net/images/magnify.gif" id="searchListButton" src="https://cdn.myanimelist.net/images/magnify.gif">
-          <input type="hidden" id="listUserId" value="7541105">
-          <input type="hidden" id="listUserName" value="Example">
-          <input type="hidden" id="listType" value="anime">
-        </div>
-      </td>
-    </tr>
-  </tbody>`,
+					</div>
+				</td>
+				<td id="mal_cs_links">
+					<div>
+						<a onclick="return false;" href="/addtolist.php?hidenav=1" class="List_LightBox">Add to List</a>
+						<a onclick="return false;" href="/">Home</a>
+					</div>
+					<div>
+						<a onclick="return false;" href="/animelist/Example">Anime List</a>
+						<a onclick="return false;" href="/mangalist/Example">Manga List</a>
+					</div>
+				</td>
+				<td id="mal_cs_otherlinks">
+											<div>
+							<strong>
+								You are viewing your anime list
+							</strong>
+						</div>
+						<div>
+							<a onclick="return false;" href="/history/Example">History</a>
+							<a onclick="return false;" href="/forum/">Forum</a>
+							<a onclick="return false;" href="/panel.php?go=export">Export</a>
+						</div>
+									</td>
+						<td id="mal_cs_powered" valign="top" align="right">
+				<a onclick="return false;" href="/"><img class=" lazyloaded" data-src="https://cdn.myanimelist.net/images/list-top-powered.gif" src="https://cdn.myanimelist.net/images/list-top-powered.gif"></a>
+				<div id="search">
+					<input id="searchBox" value="Search" type="textbox">
+					<img class=" lazyloaded" data-src="https://cdn.myanimelist.net/images/magnify.gif" id="searchListButton" src="https://cdn.myanimelist.net/images/magnify.gif">
+					<input type="hidden" id="listUserId" value="7541105">
+					<input type="hidden" id="listUserName" value="Example">
+					<input type="hidden" id="listType" value="anime">
+				</div>
+			</td>
+		</tr>
+	</tbody>`,
 
 	'visitor:user': `<tbody><tr>
-                      <td id="mal_cs_listinfo">
-          <div>
-            <strong>
-              <a onclick="return false;" href="/profile/Example">
-                <strong>Example</strong>
-              </a>
-            </strong>
-          </div>
-          <div>
-            <form action="https://myanimelist.net/logout.php" method="post">
-  <a onclick="return false;" href="javascript:void(0);" onclick="$(this).parent().submit();" class="logout">
-    <i class="fa-solid fa-fw fa-right-from-bracket"></i>
-    Logout
-  </a>
+											<td id="mal_cs_listinfo">
+					<div>
+						<strong>
+							<a onclick="return false;" href="/profile/Example">
+								<strong>Example</strong>
+							</a>
+						</strong>
+					</div>
+					<div>
+						<form action="https://myanimelist.net/logout.php" method="post">
+	<a onclick="return false;" href="javascript:void(0);" onclick="$(this).parent().submit();" class="logout">
+		<i class="fa-solid fa-fw fa-right-from-bracket"></i>
+		Logout
+	</a>
 </form>
-          </div>
-        </td>
-        <td id="mal_cs_links">
-          <div>
-            <a onclick="return false;" href="/addtolist.php?hidenav=1" class="List_LightBox">Add to List</a>
-            <a onclick="return false;" href="/">Home</a>
-          </div>
-          <div>
-            <a onclick="return false;" href="/animelist/Example">Anime List</a>
-            <a onclick="return false;" href="/mangalist/Example">Manga List</a>
-          </div>
-        </td>
-        <td id="mal_cs_otherlinks">
-                      <div>
-              <strong>
-                You are viewing anime list of
-                <a onclick="return false;" href="/profile/Example">Example</a>
-              </strong>
-            </div>
-            <div>
-              4
-                              <a onclick="return false;" href="/sharedanime.php?u1=Example&amp;u2=Example">Shared Anime</a>,
-                            <span title="Affinity">0% Affinity</span>
-              -
-                              <a onclick="return false;" href="/mangalist/Example">Manga List</a>
-                            <a onclick="return false;" href="/history/Example">History</a>
-            </div>
-                  </td>
-            <td id="mal_cs_powered" valign="top" align="right">
-        <a onclick="return false;" href="/"><img class="lazyload" data-src="https://cdn.myanimelist.net/images/list-top-powered.gif"></a>
-        <div id="search">
-          <input id="searchBox" value="Search" type="textbox">
-          <img class="lazyload" data-src="https://cdn.myanimelist.net/images/magnify.gif" id="searchListButton">
-          <input type="hidden" id="listUserId" value="7541105">
-          <input type="hidden" id="listUserName" value="Example">
-          <input type="hidden" id="listType" value="anime">
-        </div>
-      </td>
-    </tr>
-  </tbody>`,
+					</div>
+				</td>
+				<td id="mal_cs_links">
+					<div>
+						<a onclick="return false;" href="/addtolist.php?hidenav=1" class="List_LightBox">Add to List</a>
+						<a onclick="return false;" href="/">Home</a>
+					</div>
+					<div>
+						<a onclick="return false;" href="/animelist/Example">Anime List</a>
+						<a onclick="return false;" href="/mangalist/Example">Manga List</a>
+					</div>
+				</td>
+				<td id="mal_cs_otherlinks">
+											<div>
+							<strong>
+								You are viewing anime list of
+								<a onclick="return false;" href="/profile/Example">Example</a>
+							</strong>
+						</div>
+						<div>
+							4
+															<a onclick="return false;" href="/sharedanime.php?u1=Example&amp;u2=Example">Shared Anime</a>,
+														<span title="Affinity">0% Affinity</span>
+							-
+															<a onclick="return false;" href="/mangalist/Example">Manga List</a>
+														<a onclick="return false;" href="/history/Example">History</a>
+						</div>
+									</td>
+						<td id="mal_cs_powered" valign="top" align="right">
+				<a onclick="return false;" href="/"><img class="lazyload" data-src="https://cdn.myanimelist.net/images/list-top-powered.gif"></a>
+				<div id="search">
+					<input id="searchBox" value="Search" type="textbox">
+					<img class="lazyload" data-src="https://cdn.myanimelist.net/images/magnify.gif" id="searchListButton">
+					<input type="hidden" id="listUserId" value="7541105">
+					<input type="hidden" id="listUserName" value="Example">
+					<input type="hidden" id="listType" value="anime">
+				</div>
+			</td>
+		</tr>
+	</tbody>`,
 
 	'visitor:guest': `<tbody><tr>
-                      <td id="mal_cs_pic"><a onclick="return false;" href="/"><img class=" lazyloaded" data-src="https://cdn.myanimelist.net/images/list-top-logo.gif" src="https://cdn.myanimelist.net/images/list-top-logo.gif"></a></td>
-        <td id="mal_cs_otherlinks">
-          <div>
-            <strong>
-              You are viewing anime list of
-              <a onclick="return false;" href="/profile/Example">Example</a>
-            </strong>
-          </div>
-          <div>
-            <a onclick="return false;" href="/login.php">Log in</a>
-            <a onclick="return false;" href="/register.php">Create an Anime List</a>
-            <a onclick="return false;" href="/forum/?board=515949">Learn more</a>
-          </div>
-        </td>
-            <td id="mal_cs_powered" valign="top" align="right">
-        <a onclick="return false;" href="/"><img class=" lazyloaded" data-src="https://cdn.myanimelist.net/images/list-top-powered.gif" src="https://cdn.myanimelist.net/images/list-top-powered.gif"></a>
-        <div id="search">
-          <input id="searchBox" value="Search" type="textbox">
-          <img class=" lazyloaded" data-src="https://cdn.myanimelist.net/images/magnify.gif" id="searchListButton" src="https://cdn.myanimelist.net/images/magnify.gif">
-          <input type="hidden" id="listUserId" value="7541105">
-          <input type="hidden" id="listUserName" value="Example">
-          <input type="hidden" id="listType" value="anime">
-        </div>
-      </td>
-    </tr>
-  </tbody>`
+											<td id="mal_cs_pic"><a onclick="return false;" href="/"><img class=" lazyloaded" data-src="https://cdn.myanimelist.net/images/list-top-logo.gif" src="https://cdn.myanimelist.net/images/list-top-logo.gif"></a></td>
+				<td id="mal_cs_otherlinks">
+					<div>
+						<strong>
+							You are viewing anime list of
+							<a onclick="return false;" href="/profile/Example">Example</a>
+						</strong>
+					</div>
+					<div>
+						<a onclick="return false;" href="/login.php">Log in</a>
+						<a onclick="return false;" href="/register.php">Create an Anime List</a>
+						<a onclick="return false;" href="/forum/?board=515949">Learn more</a>
+					</div>
+				</td>
+						<td id="mal_cs_powered" valign="top" align="right">
+				<a onclick="return false;" href="/"><img class=" lazyloaded" data-src="https://cdn.myanimelist.net/images/list-top-powered.gif" src="https://cdn.myanimelist.net/images/list-top-powered.gif"></a>
+				<div id="search">
+					<input id="searchBox" value="Search" type="textbox">
+					<img class=" lazyloaded" data-src="https://cdn.myanimelist.net/images/magnify.gif" id="searchListButton" src="https://cdn.myanimelist.net/images/magnify.gif">
+					<input type="hidden" id="listUserId" value="7541105">
+					<input type="hidden" id="listUserName" value="Example">
+					<input type="hidden" id="listType" value="anime">
+				</div>
+			</td>
+		</tr>
+	</tbody>`
 }
 
 function setView(view) {

assets/preview_common.js
@@ -39,6 +39,9 @@ window.addEventListener(
 			else if(type === 'cover') {
 				toggleCover(content);
 			}
+			else if(type === 'category') {
+				changeCategory(content);
+			}
 			else {
 				console.log('[receiver.js] Malformed request received. No action taken.')
 			}

commit 008bba3fb6607ab73a6dd9c73d1074d8be970805

Fix modern category buttons not applying "on" class
Regression from a0e477304f35c09d0f6540462b6479f821141ce3

assets/preview_modern.js
@@ -38,9 +38,9 @@ function changeCategory(catId, catBtn) {
 	}
 	
 	// Change basic attributes
-	catBtn.classList.add('on');
 	document.body.setAttribute('data-query',`{"status":${catId}}`);
 	document.querySelector('.status-button.on').classList.remove('on');
+	catBtn.classList.add('on');
 	document.getElementsByClassName('list-unit')[0].className = `list-unit ${catBtn.className.substring(14)}`;
 	document.querySelector('.list-status-title .text').textContent = categoryNames[catId].toUpperCase();
 