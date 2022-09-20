ValerioLyndon/Theme-Customiser

commit 61ac527c174932f7f9f93a90950d909e8861b952

Better filters & browse page (#45) / JSON v0.3
In this commit:
• JSON version bumped to 0.3.
• "tags" keys can now be dictionaries instead of arrays, allowing for categories.
• Multiple tags can now be selected using "AND" logic.
• Added search for titles on browse page.
• Sort, tags, and search are all saved in the URL for easy linking and so that the page will remember on refresh.
• Improved style of tag clouds.

assets/browse.js
@@ -2,132 +2,209 @@
 // COMMON FUNCTIONS
 // ================
 
-// Capitalises the first letter of every word. To capitalise sentences, set the divider to ".".
-function capitalise(str, divider = ' ') {
-	let words = str.split(divider);
-	
-	for(i = 0; i < words.length; i++) {
-		let first = words[i].substring(0,1).toUpperCase(),
-			theRest = words[i].substring(1);
-		words[i] = first + theRest;
-	}
-	
-	str = words.join(divider);
-	return str;
-}
+// An extended filter class with search & sorting
+class ExtendedFilters extends BaseFilters {
+	constructor( items, selector = 'ID' ){
+		super( items, selector );
 
-// Sort function
+		// Search Variables
+		this.searchBar = document.getElementById('js-search');
+		this.searchAttributes = ['data-title'];
+		this.itemSearchCls = 'is-hidden-by-search';
 
-/* Activated by user on click of a button. Updates relevant UI elements then call sortItems. 
- * link
-   - the clicked link/element
- * array
-   - an array of arguments to pass directly to sortItems
- */
-function selectSort(link, array) {
-	// Add sort value to array if needed
-	if(!array[2]) {
-		array[2] = 'ascending';
-	}
-
-	// Check if currently sorted
-	let currentSort = link.getAttribute('data-sort');
-
-	// Remove all other sort styling
-	let sortLinks = document.getElementsByClassName('js-sort');
-	for(let l of sortLinks) {
-		l.classList.remove('is-active');
-		l.removeAttribute('data-sort');
-
-		let icons = l.getElementsByClassName('js-sort-icon');
-		for(let icon of icons) {
-			icon.classList.add('o-hidden');
+		// Sort Variables
+		this.sortContainer = document.getElementById('js-sorts');
+		this.activeSort = [];
+		this.sorts = {
+			'title': {
+				'attr': 'data-title',
+				'default': 'ascending',
+				'label': 'Title'
+			},
+			'author': {
+				'attr': 'data-author',
+				'default': 'ascending',
+				'label': 'Author'
+			},
+			'date': {
+				'attr': 'data-date',
+				'default': 'descending',
+				'label': 'Release Date'
+			},
+			'random': {
+				'attr': 'random',
+				'label': 'Random'
+			}
 		}
 	}
 
-	// Correct array sorting if needed
-	if(array[2] === 'descending' && currentSort === 'descending') {
-		array[2] = 'ascending';
-	} else if(array[2] === 'ascending' && currentSort === 'ascending') {
-		array[2] = 'descending';
-	}
-	
-	// Add styling to current item
-	link.classList.add('is-active');
-	try {
-		let cls = array[2] === 'descending' ? 'js-descending' : 'js-ascending';
-		link.getElementsByClassName(cls)[0].classList.remove('o-hidden');
-	} catch {}
-	link.setAttribute('data-sort', array[2]);
+	renderSorts( ){
+		for( let [key, info] of Object.entries(this.sorts) ){
+			// Check that sort is valid and delete if not
+			let valid = false;
+			if(info['attr'] !== 'random') {
+				for( let item of this.items ){
+					if( item.hasAttribute(info['attr']) ){
+						valid = true;
+						break;
+					}
+				}
+				if(!valid) {
+					delete this.sorts[key];
+					continue;
+				}
+			}
 
-	// Sort items.
-	sortItems(...array);
-}
+			// Render HTML
+			let div = document.createElement('div'),
+				link = document.createElement('a'),
+				icon = document.createElement('i');
+			div.className = 'dropdown__item';
+			link.className = 'hyper-button';
+			link.id = `sort:${key}`;
+			link.textContent = `${info['label']} `;
+			icon.className = 'hyper-button__icon fa-solid fa-sort-asc o-hidden';
 
-// "items" var should be a DOM node list
-function sortItems(items, attribute, order = 'ascending') {
-	let attributes = [];
+			link.appendChild(icon);
+			div.appendChild(link);
+			this.sortContainer.appendChild(div);
 
-	for(let it of items) {
-		let value = it.getAttribute(attribute),
-			id = it.id.split(':')[1];
-		attributes.push([value, id]);
-	}
+			this.sorts[key]['btn'] = link;
+			this.sorts[key]['icon'] = icon;
 
-	if(attribute === 'random') {
-		let currentIndex = attributes.length, randomIndex;
-
-		// While there remain elements to shuffle.
-		while (currentIndex != 0) {
-
-			// Pick a remaining element.
-			randomIndex = Math.floor(Math.random() * currentIndex);
-			currentIndex--;
-
-			// And swap it with the current element.
-			[attributes[currentIndex], attributes[randomIndex]] = [
-			attributes[randomIndex], attributes[currentIndex]];
+			link.addEventListener('click', () => {
+				this.sort(key);
+			});
 		}
-	} else {
-		attributes.sort((attrOne,attrTwo) => {
-			a = attrOne[0].toLowerCase();
-			b = attrTwo[0].toLowerCase();
-			if(a < b && order === 'ascending' || a > b && order === 'descending') { return -1; }
-			if(a > b && order === 'ascending' || a < b && order === 'descending') { return 1; }
-			return 0;
-		});
 	}
 
-	// Apply sort order
-	for(i = 0; i < attributes.length; i++) {
-		let id = attributes[i][1];
-		document.getElementById(`card:${id}`).style.order = i;
+	renderSearch( ){
+		this.searchBar.classList.remove('o-hidden');
+		this.searchBar.addEventListener('input', () => { this.search(this.searchBar.value); } );
+	}
+
+	reset( ){
+		this.resetTags();
+		this.resetSearch();
+	}
+	resetSearch( ){
+		query.remove('search');
+		
+		for( let item of this.items ){
+			item.classList.remove(this.itemSearchCls);
+		}
+	}
+
+	// Search
+	search( input ){
+		if( input.length > 0 ){
+			query.set('search', input);
+		}
+		else {
+			query.remove('search');
+		}
+
+		for( let item of this.items ){
+			let match = false;
+			for( let attr of this.searchAttributes ){
+				let attrValue = item.getAttribute(attr);
+
+				if( attrValue && attrValue.toLowerCase().includes( input.toLowerCase() ) ){
+					match = true;
+					break;
+				}
+			}
+			if( match ){
+				item.classList.remove(this.itemSearchCls);
+			}
+			else {
+				item.classList.add(this.itemSearchCls);
+			}
+		}
+	}
+
+	sort( key, forceOrder, updateQuery = true ) {
+		let info = this.sorts[key];
+		// returns false if sort key is invalid
+		if(!info) {
+			return false;
+		}
+
+		let attributes = [],
+			order = forceOrder ? forceOrder : info['default'];
+
+		// check if already sorted
+		if( this.activeSort.length > 0 ){
+			if( !(this.activeSort[0] === key) ) {
+				this.sorts[this.activeSort[0]]['btn'].classList.remove('is-active');
+				this.sorts[this.activeSort[0]]['icon'].classList.add('o-hidden');
+			}
+			else if( this.activeSort[1] === order ){
+				order = (order === 'ascending') ? 'descending' : 'ascending';
+			}
+		}
+
+		// update button
+		info['btn'].classList.add('is-active');
+		if( key !== 'random' ) {
+			info['icon'].classList.remove('o-hidden', 'fa-sort-asc', 'fa-sort-desc');
+			if( order === 'ascending' ){
+				info['icon'].classList.add('fa-sort-asc');
+			}
+			else {
+				info['icon'].classList.add('fa-sort-desc');
+			}
+		}
+
+		// calculate sort
+		for( let item of this.items ) {
+			let value = item.getAttribute(info['attr']),
+				id = item.id;
+			attributes.push([value, id]);
+		}
+
+		if( key === 'random' ){
+			let currentIndex = attributes.length, randomIndex;
+
+			// While there remain elements to shuffle.
+			while (currentIndex != 0) {
+
+				// Pick a remaining element.
+				randomIndex = Math.floor(Math.random() * currentIndex);
+				currentIndex--;
+
+				// And swap it with the current element.
+				[attributes[currentIndex], attributes[randomIndex]] = [
+				attributes[randomIndex], attributes[currentIndex]];
+			}
+		}
+		else {
+			attributes.sort((attrOne,attrTwo) => {
+				let a = attrOne[0].toLowerCase();
+				let b = attrTwo[0].toLowerCase();
+				if(a < b && order === 'ascending' || a > b && order === 'descending') { return -1; }
+				if(a > b && order === 'ascending' || a < b && order === 'descending') { return 1; }
+				return 0;
+			});
+		}
+
+		// Apply sort, set URL query, update variables
+
+		for( i = 0; i < attributes.length; i++ ){
+			let id = attributes[i][1];
+			document.getElementById(id).style.order = i;
+		}
+
+		if(updateQuery) {
+			query.set('sort', key);
+			query.set('sortdir', order);
+		}
+
+		this.activeSort = [key, order];
+
+		return true;
 	}
 }
-
-function pushTag(thisId, tag, category = 'other') {
-	if(!tags[category][tag]) {
-		tags[category][tag] = [];
-	}
-	tags[category][tag].push(thisId);
-}
-
-// sorts a dictionary by key
-function sortKeys(dict) {
-	let keys = Object.keys(dict);
-	keys.sort((a,b) => {
-		return a.toLowerCase().localeCompare(b.toLowerCase());
-	});
-
-	let sorted = {};
-	for(let k of keys) {
-		sorted[k] = dict[k];
-	}
-
-	return sorted;
-}
-
-
 
 // ==================
 // ONE-TIME FUNCTIONS
@@ -145,7 +222,7 @@ function renderCards(cardData) {
 		}
 
 		let cardParent = document.createElement('a');
-		cardParent.className = 'browser__card js-card';
+		cardParent.className = 'browser__card';
 		let cardUrl = `./theme?t=${theme['url']}`;
 		if(collectionUrls.length > 0) {
 			cardUrl += `&c=${collectionUrls.join('&c=')}`;
@@ -172,9 +249,6 @@ function renderCards(cardData) {
 
 			cardParent.setAttribute('data-author', theme['author']);
 		}
-
-		let themeTags = theme['tags'] ? theme['tags'] : [];
-		cardParent.setAttribute('data-tags', themeTags);
 
 		let card = document.createElement('div');
 		card.className = 'card';
@@ -243,16 +317,17 @@ function renderCards(cardData) {
 		}
 
 		document.getElementById('js-theme-list').appendChild(cardParent);
+		cards.push(cardParent);
 
 		// Add tags to sortable list
-		pushTag(thisId, theme['type'], 'list type');
-		pushTag(thisId, themeAuthor, 'author');
-		for(let tag of themeTags) {
-			let category = 'other';
-			if(['card layout', 'cover layout', 'table layout'].includes(tag)) {
-				category = 'layout';
+		tempTags = formatFilters(theme['tags']);
+		tempTags['list type'] = [theme['type']];
+		tempTags['author'] = [themeAuthor];
+
+		for( let [category, tags] of Object.entries(tempTags) ){
+			for( let tag of tags ){
+				pushFilter(thisId, tag, category);
 			}
-			pushTag(thisId, tag, category);
 		}
 		itemCount++;
 	}
@@ -264,15 +339,9 @@ function renderCards(cardData) {
 
 // Variables
 
-var tags = {
-		"list type": {},
-		"layout": {},
-		"author": {},
-		"source": {},
-		"other": {}
-	},
-	itemCount = 0,
-	sorts = ['data-title'];
+var itemCount = 0,
+	sorts = ['data-title'],
+	cards = [];
 
 // Get data for all collections and call other functions
 
@@ -353,53 +422,58 @@ fetchAllFiles(megaUrls)
 				renderCards(json['themes']);
 			}
 
-			loader.text('Sorting items...');
+			loader.text('Filtering items...');
 
-			function renderBrowseTags(categoryName, categoryTags) {
-
-				let cloudEle = document.getElementById('js-tags__cloud');
-
-				let header = document.createElement('div');
-				header.textContent = capitalise(categoryName);
-				header.className = 'tag-cloud__header';
-				cloudEle.appendChild(header);
-
-				renderTags(categoryTags, [...Array(itemCount).keys()], 'card:ID');
-			}
-
+			// Create and load filters.
 			if(itemCount > 5) {
-				for(let [categoryName, categoryTags] of Object.entries(tags)) {
+				var filter = new ExtendedFilters(cards, 'card:ID');
+
+				let hasTags = false;
+				for(let categoryTags of Object.values(tags)) {
 					if(Object.keys(categoryTags).length > 0) {
-						categoryTags = sortKeys(categoryTags);
-						renderBrowseTags(categoryName, categoryTags);
+						hasTags = true;
+						break;
+					}
+				}
+				if( hasTags ){
+					filter.renderTags(tags);
+				}
+
+				// Add sort functionality
+				filter.renderSorts();
+
+				// Add search functionality
+				filter.renderSearch();
+
+				let tSearch = query.get('search'),
+					tTags = query.get('tags');
+				
+				if( tSearch ){
+					filter.search( tSearch );
+					filter.searchBar.value = tSearch;
+				}
+				if( tTags ){
+					let splitTags = tTags.split('&&');
+					for( let tag of splitTags ){
+						document.getElementById(`tag:${tag}`).dispatchEvent( new Event('click') );
 					}
 				}
 			}
 
 			// Add sort dropdown items and apply default sort
-			var cards = document.getElementsByClassName('js-card');
+			let attemptedSort = false,
+				tSort = query.get('sort'),
+				tSortDir = query.get('sortdir') ? query.get('sortdir') : 'ascending';
 			
-			let titleLink = document.getElementById('js-sort-title')
-			titleLink.addEventListener('click', () => { selectSort(titleLink, [cards, 'data-title']) });
-			
-			let dataLink = document.getElementById('js-sort-date')
-			if(sorts.includes('data-date')) {
-				dataLink.addEventListener('click', () => { selectSort(dataLink, [cards, 'data-date', 'descending']) });
-				sortItems(cards, 'data-date', 'descending');
-			} else {
-				dataLink.parentNode.remove();
-				sortItems(cards, 'random');
+			if( tSort ){
+				attemptedSort = filter.sort(tSort, tSortDir, false);
 			}
-			
-			let authorLink = document.getElementById('js-sort-author')
-			if(sorts.includes('data-author')) {
-				authorLink.addEventListener('click', () => { selectSort(authorLink, [cards, 'data-author']) });
-			} else {
-				authorLink.parentNode.remove();
+			else {
+				attemptedSort = filter.sort('date', undefined, false)
 			}
-			
-			let randomLink = document.getElementById('js-sort-random')
-			randomLink.addEventListener('click', () => { selectSort(randomLink, [cards, 'random']) });
+			if( attemptedSort === false ){
+				filter.sort('random', undefined, false);
+			}
 
 			if(failures >= files.length) {
 				loader.failed(['Encountered a problem while parsing theme information.', 'json.parse']);

assets/common.js
@@ -260,96 +260,343 @@ function toggleEle(selector, btn = false, set = undefined) {
 	}
 }
 
+// Capitalises the first letter of every word. To capitalise sentences, set the divider to ".".
+function capitalise(str, divider = ' ') {
+	let words = str.split(divider);
+	
+	for(i = 0; i < words.length; i++) {
+		let first = words[i].substring(0,1).toUpperCase(),
+			theRest = words[i].substring(1);
+		words[i] = first + theRest;
+	}
+	
+	str = words.join(divider);
+	return str;
+}
+
+// sorts a dictionary by key
+function sortKeys(dict) {
+	let keys = Object.keys(dict);
+	keys.sort((a,b) => {
+		return a.toLowerCase().localeCompare(b.toLowerCase());
+	});
+
+	let sorted = {};
+	for(let k of keys) {
+		sorted[k] = dict[k];
+	}
+
+	return sorted;
+}
+
 // Tag Functionality & Renderer
 
-let tagsBtn = document.getElementById('js-tags__button');
+// Tag variables
 
-/* Adds functional tags to the HTML. Accepts three values:
- * tags
-   - A dictionary containing tag keys and their id arrays
-   e.x { "my tag": ["item id", 0, "three"] }
- * allIds
-   - an array of all ID values to show/hide when selecting tags.
-   e.x ["my mod", "mod 2", "image mod"]
- * selector
-   - a CSS ID to target the correct items using IDs from allIds.
-   - will replace the "ID" text during runtime.
-   e.x "card:ID"
- */
-function renderTags(tags, allIds, selector) {
-	tagsBtn.classList.remove('o-hidden');
+var tags = {};
 
-	let cloudEle = document.getElementById('js-tags__cloud');
-
-	for(let [tag, itemIds] of Object.entries(tags)) {
-		let tagEle = document.createElement('button'),
-			countEle = document.createElement('span'),
-			count = itemIds.length;
-
-		tagEle.textContent = tag;
-		tagEle.className = 'tag-cloud__tag js-tag';
-		tagEle.setAttribute('data-items', itemIds);
-
-		countEle.textContent = count;
-		countEle.className = 'tag-cloud__count';
-		tagEle.appendChild(countEle);
-		cloudEle.appendChild(tagEle);
-
-		// Add tag button functions
-		tagEle.addEventListener('click', () => { selectTag(tagEle, allIds, selector); });
+function formatFilters( filters ){
+	if( filters instanceof Array ){
+		return {'other': filters};
 	}
+	if( filters instanceof Object ){
+		return filters;
+	}
+	return {};
 }
 
-function selectTag(tagEle, allIds, selector) {
-	// Clear previous selection
-	let hidden = document.querySelectorAll('.is-hidden-by-tag'),
-		isSelected = tagEle.className.includes('is-selected');
-	for(let ele of hidden) {
-		ele.classList.remove('is-hidden-by-tag');
+function pushFilter(thisId, tag, category = 'other') {
+	if( !tags[category] ){
+		tags[category] = [];
 	}
-
-	// Remove other tags' styling & set our own
-	tagsBtn.classList.remove('has-selected');
-	let selectedTags = document.querySelectorAll('.js-tag.is-selected');
-
-	for(let tag of selectedTags) {
-		tag.classList.remove('is-selected');
+	if( !tags[category][tag] ){
+		tags[category][tag] = [];
 	}
+	tags[category][tag].push(thisId);
+}
 
-	// Select new tags
-	if(!isSelected) {
-		tagsBtn.classList.add('has-selected');
-		let itemsToKeep = tagEle.getAttribute('data-items').split(',');
+/* Adds functional tags to the HTML.
+ | 
+ | Constructor must be fed:
+ | • a NodeList or array of Nodes
+ | • a dictionary of filter/ID pairs. Example:
+ |   {
+ |     "My Tag": [0, 3, 12, 32],
+ |     …
+ |   }
+ | • a string selector with "ID" in place of tag ID. E.x:
+ |   "card:ID" or "mod:ID"
+ |
+ | Also requires two elements in the HTML:
+ | • A button with ID 'js-tags__button'
+ | • A div with ID 'js-tags__cloud'
+ */
+class BaseFilters {
+	constructor( items, selector = 'ID' ){
+		// Variables for all
+		this.toggle = document.getElementById('js-tags__button');
+		this.toggleCls = 'has-selected';
+		this.clearBtn = document.getElementById('js-tags__clear');
+		this.items = [...items];
 
-		for(let i = 0; i < itemsToKeep.length; i++) {
-			itemsToKeep[i] = String(itemsToKeep[i]);
+		// Tag Variables
+		this.tagContainer = document.getElementById('js-tags__cloud');
+		this.buttons = [];
+		this.selectedButtons = [];
+		this.selectedTags = {};
+		this.btnCls = 'is-selected';
+		this.itemTagCls = 'is-hidden-by-tag';
+
+		// Other Variables
+		this.selector = selector;
+
+		// Create Meta Buttons
+		if( this.clearBtn ){
+			this.clearBtn.classList.remove('o-hidden');
+			this.clearBtn.addEventListener('click', () => {
+				this.reset();
+			});
 		}
-		
-		for(let id of allIds) {
-			id = String(id);
-			
-			if(itemsToKeep.includes(id)) {
-				continue;
+	}
+
+	renderTags( tags ){
+		this.toggle.classList.remove('o-hidden');
+
+		let tagCategories = Object.entries(tags);
+		for( let [category, tags] of tagCategories ){
+			let totalInCategory = 0;
+
+			let group = document.createElement('div');
+			group.className = 'tag-cloud__group';
+			if(category === 'other') {
+				group.style.order = 100;
+			} else if(category === 'list type') {
+				group.style.order = 1;
+				group.classList.add('tag-cloud__group--column');
+			} else if(category === 'layout') {
+				group.style.order = 2;
+				group.classList.add('tag-cloud__group--column');
 			} else {
-				document.getElementById(selector.replace('ID', id)).classList.add('is-hidden-by-tag');
+				group.style.order = 50;
+			}
+			this.tagContainer.appendChild(group);
+
+			let header = document.createElement('div');
+			if( tagCategories.length > 1 ){
+				header.textContent = capitalise(category);
+				header.className = 'tag-cloud__header';
+				group.appendChild(header);
+			}
+
+			// Sort filters ascending
+			tags = sortKeys(tags);
+
+			// Create Filter Buttons
+			for(let [tag, itemIds] of Object.entries(tags)) {
+				let button = document.createElement('button'),
+					countEle = document.createElement('span'),
+					count = itemIds.length;
+
+				// skip rendering tag if all items match, thus making it useless
+				if( count === this.items.length ){
+					continue;
+				} else {
+					totalInCategory++;
+				}
+
+				button.textContent = tag;
+				button.className = 'tag-cloud__tag';
+				button.id = `tag:${tag}`;
+
+				// count of items
+				countEle.textContent = count;
+				countEle.className = 'tag-cloud__count';
+				button.appendChild(countEle);
+				group.appendChild(button);
+
+				// format Ids
+				for( let i = 0; i < itemIds.length; i++ ) {
+					itemIds[i] = this.formatId(itemIds[i]);
+				}
+
+				this.buttons.push({
+					'btn': button,
+					'count': countEle,
+					'ids': itemIds,
+					'total': count
+				});
+
+				// Add tag button functions
+				button.addEventListener('click', () => { this.activateTag(button, tag, itemIds); });
+			}
+
+			// If category is empty, skip
+			if( totalInCategory === 0 ){
+				group.remove();
 			}
 		}
-		tagEle.classList.add('is-selected');
+	}
+
+	reset( ){
+		this.resetTags();
+	}
+	resetTags( ){
+		query.remove('tags');
+
+		for( let item of this.items ){
+			item.classList.remove(this.itemTagCls);
+		}
+
+		this.toggle.classList.remove(this.toggleCls);
+		for( let btn of this.buttons ){
+			btn['btn'].classList.remove('is-disabled', 'is-selected');
+			btn['count'].textContent = btn['total'];
+		}
+		this.selectedButtons = [];
+		this.selectedTags = [];
+	}
+
+	// ID Formatting
+	formatId( id ) {
+		return this.selector.replace('ID', id);
+	}
+
+	// On button click
+	activateTag( button, itemName, itemIds ){
+		// Check if already selected and select button if not
+		let tagQ = query.get('tags'),
+			tagSplit = tagQ ? tagQ.split('&&') : [],
+			tagIndex = tagSplit.indexOf(itemName);
+		
+		let selected = this.selectedButtons.indexOf(button);
+		if( selected !== -1 ){
+			button.classList.remove(this.btnCls);
+			this.selectedButtons.splice(selected, 1);
+			delete this.selectedTags[itemName];
+
+			// Remove from URL
+			if( tagIndex !== -1 ) {
+				tagSplit.splice(tagIndex, 1);
+				query.set('tags', tagSplit.join('&&'));
+			}
+		}
+		else {
+			this.toggle.classList.add(this.toggleCls);
+			button.classList.add(this.btnCls);
+			this.selectedButtons.push(button);
+			this.selectedTags[itemName] = itemIds;
+
+			// Add to URL
+			if( tagIndex === -1 ) {
+				tagSplit.push(itemName);
+				query.set('tags', tagSplit.join('&&'));
+			}
+		}
+
+		// If nothing is selected anymore, clear all.
+		if( this.selectedButtons.length === 0 ){
+			this.resetTags();
+			return;
+		}
+		
+		// Calculate new filter based on selected buttons
+		let filterCount = {};
+		for( let filter of Object.values(this.selectedTags) ){
+			for( let id of filter ){
+				if( !Object.keys(filterCount).includes(id) ){
+					filterCount[id] = 1;
+				} else {
+					filterCount[id] += 1;
+				}
+			}
+		}
+		let orFilters = Object.keys(filterCount),
+			andFilters = [];
+		// create AND filter by only adding filters that match all of the selected filters
+		for( let [id, count] of Object.entries(filterCount) ){
+			if( count === Object.keys(this.selectedTags).length ){
+				andFilters.push(id);
+			}
+		}
+
+		// Show matching items
+		for( let item of this.items ){
+			if( andFilters.includes(item.id) ) {
+				item.classList.remove(this.itemTagCls);
+			}
+			else {
+				item.classList.add(this.itemTagCls);
+			}
+		}
+
+		// Update buttons
+		for( let btn of this.buttons ){
+			let crossover = 0;
+			for( let id of andFilters ){
+				if( btn['ids'].includes(id) ){
+					crossover++;
+				}
+			}
+			btn['count'].textContent = crossover;
+			if( crossover === 0 ){
+				btn['btn'].classList.add('is-disabled');
+			}
+		}
 	}
 }
 
+// URL class for easy setting and changing of current location.
+const query = new class ActiveURLParams {
+	constructor( ){
+		this.url = new URL(document.location);
+		this.params = this.url.searchParams;
+		// aliases
+		this.remove = this.delete;
+		this.add = this.set;
+	}
 
+	has( ){
+		return this.params.has(...arguments);
+	}
+	get( ){
+		return this.params.get(...arguments);
+	}
+	getAll( ){
+		return this.params.getAll(...arguments);
+	}
+	entries( ){
+		return this.params.entries(...arguments);
+	}
+	append( ){
+		this.params.append(...arguments);
+		this.updateUrl();
+	}
+	set( ){
+		this.params.set(...arguments);
+		this.updateUrl();
+	}
+	delete( ){
+		this.params.delete(...arguments);
+		this.updateUrl();
+	}
+
+	updateUrl( ){
+		history.replaceState(null, '', this.url.href);
+	}
+	gotoUrl( ){
+		window.location = this.url.href;
+	}
+};
 
 // VARIABLES
 
 const
-	query = (new URL(document.location)).searchParams,
 	megaUrls = query.getAll('m'),
 	collectionUrls = query.getAll('c'),
 	themeUrls = query.getAll('t'),
 	loader = new loadingScreen(),
 	messenger = new messageHandler(),
-	jsonVersion = 0.2;
+	jsonVersion = 0.3;
 
 
 
@@ -372,7 +619,8 @@ async function processJson(json, url, toReturn) {
 
 	// Else, continue to process.
 	if(ver > jsonVersion) {
-		console.log('Detected JSON version ahead of current release. Processing as normal.');
+		messenger.send('Detected JSON version beyond what is supported by this instance. Attempting to process as normal. If any bugs or failures occur, try using the main instance at valeriolyndon.github.io.');
+		console.log('Detected JSON version beyond what is supported by this instance. Attempting to process as normal. If any bugs or failures occur, try updating your fork from the main instance at valeriolyndon.github.io.');
 	}
 
 	else if(ver < jsonVersion) {

assets/customiser.js
@@ -1100,11 +1100,11 @@ function renderCustomisation(entryType, entry, parentEntry = [undefined, undefin
 
 		// Add mod tag to list of tags
 		if('tags' in entryData) {
-			for(let tag of entryData['tags']) {
-				if(modTags[tag]) {
-					modTags[tag].push(entryId);
-				} else {
-					modTags[tag] = [entryId];
+			tempTags = formatFilters(entryData['tags']);
+
+			for( let [category, tags] of Object.entries(tempTags) ){
+				for( let tag of tags ){
+					pushFilter(entryId, tag, category);
 				}
 			}
 		}
@@ -1183,6 +1183,7 @@ function pageSetup() {
 		optionsEle.parentNode.remove();
 	}
 
+	let mods = [];
 	let modsEle = document.getElementById('js-mods');
 	if('mods' in theme) {
 		for (const mod of Object.entries(theme['mods'])) {
@@ -1190,6 +1191,7 @@ function pageSetup() {
 			if(typeof renderedMod === 'string') {
 				console.log(`[ERROR] Skipped mod "${modId}": ${renderedMod}`);
 			} else {
+				mods.push(renderedMod);
 				modsEle.appendChild(renderedMod);
 			}
 		}
@@ -1198,8 +1200,9 @@ function pageSetup() {
 	}
 
 	// Tag links
-	if(Object.entries(modTags).length > 0 && Object.entries(theme['mods']).length > 3) {
-		renderTags(modTags, Object.keys(theme['mods']), 'mod-parent:ID');
+	if(Object.entries(tags).length > 0 && Object.entries(theme['mods']).length > 3) {
+		var filter = new BaseFilters(mods, 'mod-parent:ID');
+		filter.renderTags(tags);
 	}
 
 	// Back link
@@ -1822,7 +1825,7 @@ function postToIframe(msg) {
 var theme = '',
 	json = null,
 	baseCss = '',
-	modTags = {};
+	tags = {};
 
 var userSettings = {
 	'data': themeUrls[0],

commit e734a65bf5bbecde6d5b56e5a3c03943d36c022b

Fix collections with <5 items infinitely loading.
Regression from 61ac527c174932f7f9f93a90950d909e8861b952

assets/browse.js
@@ -38,7 +38,11 @@ class ExtendedFilters extends BaseFilters {
 		}
 	}
 
-	renderSorts( ){
+	initaliseSort( display = true ){
+		if( display ){
+			document.getElementById('js-sorts-parent').classList.remove('o-hidden');
+		}
+
 		for( let [key, info] of Object.entries(this.sorts) ){
 			// Check that sort is valid and delete if not
 			let valid = false;
@@ -78,7 +82,7 @@ class ExtendedFilters extends BaseFilters {
 		}
 	}
 
-	renderSearch( ){
+	initaliseSearch( ){
 		this.searchBar.classList.remove('o-hidden');
 		this.searchBar.addEventListener('input', () => { this.search(this.searchBar.value); } );
 	}
@@ -430,10 +434,17 @@ fetchAllFiles(megaUrls)
 			}
 
 			loader.text('Filtering items...');
-
+			
 			// Create and load filters.
-			if(itemCount > 5) {
-				var filter = new ExtendedFilters(cards, 'card:ID');
+			var filter = new ExtendedFilters(cards, 'card:ID');
+
+			if( itemCount <= 5 ){
+				filter.initaliseSort(false);
+			}
+
+			else if( itemCount > 5 ){
+				filter.initaliseSort(true);
+				filter.initaliseSearch();
 
 				let hasTags = false;
 				for(let categoryTags of Object.values(tags)) {
@@ -443,14 +454,8 @@ fetchAllFiles(megaUrls)
 					}
 				}
 				if( hasTags ){
-					filter.renderTags(tags);
+					filter.initaliseTags(tags);
 				}
-
-				// Add sort functionality
-				filter.renderSorts();
-
-				// Add search functionality
-				filter.renderSearch();
 
 				let tSearch = query.get('search'),
 					tTags = query.get('tags');

assets/common.js
@@ -366,15 +366,17 @@ class BaseFilters {
 
 		// Create Meta Buttons
 		if( this.clearBtn ){
-			this.clearBtn.classList.remove('o-hidden');
 			this.clearBtn.addEventListener('click', () => {
 				this.reset();
 			});
 		}
 	}
 
-	renderTags( tags ){
+	initaliseTags( tags ){
 		this.toggle.classList.remove('o-hidden');
+		if( this.clearBtn ){
+			this.clearBtn.classList.remove('o-hidden');
+		}
 
 		let tagCategories = Object.entries(tags);
 		for( let [category, tags] of tagCategories ){

assets/customiser.js
@@ -1196,7 +1196,7 @@ function pageSetup() {
 	// Tag links
 	if(Object.entries(tags).length > 0 && Object.entries(theme['mods']).length > 3) {
 		var filter = new BaseFilters(mods, 'mod-parent:ID');
-		filter.renderTags(tags);
+		filter.initaliseTags(tags);
 	}
 
 	// Back link

index.html
@@ -35,7 +35,7 @@
 
 			<div class="filter">
 				<div class="filter__row filter__row--center">
-					<div class="dropdown">
+					<div class="dropdown o-hidden" id="js-sorts-parent">
 						<button class="button button--unusable">
 							<i class="fa-solid fa-sort"></i> Sort
 						</button>
@@ -49,8 +49,8 @@
 					</button>
 
 					<button class="button o-hidden" id="js-tags__clear" onclick="document.getElementById('js-search').value = '';">
-							<i class="fa-solid fa-xmark"></i> Clear All
-						</button>
+						<i class="fa-solid fa-xmark"></i> Clear All
+					</button>
 				</div>
 
 				<div class="filter__row">