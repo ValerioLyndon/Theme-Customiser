6ac760595005f0c3dec7d984c39c378107ab479d
Valerio Lyndon <valeriolyndon@gmail.com>
2023-03-12 17:28:20 -0700

    Attempt to add user agent analytic

--- assets/analytics.js
+++ assets/analytics.js
@@ -1,3 +1,7 @@
+gtag('event', 'user', {
+	'user_agent': window.navigator.userAgent
+})
+
 let installBtn = document.querySelector('.gtag-install')
 if( installBtn ){
 	installBtn.addEventListener('click', () => {
