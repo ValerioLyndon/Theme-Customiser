ba029f253839deee80e7035f254b92620617cb7b
Valerio Lyndon <valeriolyndon@gmail.com>
2023-03-21 21:40:38 -0700

    Fix page halting on themes without options
    Regression from 62ae4362be366a7b10e1ac524b3685e820982044

--- assets/customiser.js
+++ assets/customiser.js
@@ -582,13 +582,15 @@ async function updateCss(  ){
 
 	// Options
 	// Sort options before to match the order of the JSON to prevent issues with incorrectly layered mods
-	let sortedOptIds = sortArrayToMatch(Object.keys(userSettings.options), Object.keys(theme.options));
-	for( let optId of sortedOptIds ){
-		newCss = await applyOptionToCss(newCss, theme.options[optId], userSettings.options[optId]);
+	if( userSettings?.options && theme?.options ){
+		let sortedOptIds = sortArrayToMatch(Object.keys(userSettings.options), Object.keys(theme.options));
+		for( let optId of sortedOptIds ){
+			newCss = await applyOptionToCss(newCss, theme.options[optId], userSettings.options[optId]);
+		}
 	}
 
 	// Mods
-	if( theme.mods && Object.keys(userSettings.mods).length > 0 ){
+	if( theme?.mods && Object.keys(userSettings?.mods).length > 0 ){
 		// Sort mods before to match the order of the JSON to prevent issues with incorrectly layered mods
 		let sortedModIds = sortArrayToMatch(Object.keys(userSettings.mods), Object.keys(theme.mods));
 
