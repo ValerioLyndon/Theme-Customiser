ValerioLyndon/Theme-Customiser

commit 250bea24539c1cce1eff61c68f689a7aa5675aaa

new theme/preview keys + fixes
Theme and preview sections now support new "cover_url" and "background_url" keys to specify recommended or best-for-preview images. These key names were chosen so as to not break older JSON and give clear purposes to each key.

Also...
- fixed list background being hidden.
- fixed unstrict preview JS that was erroring.

assets/customiser.js
@@ -1225,11 +1225,15 @@ function pageSetup( ){
 	let coverHtml = document.getElementById('js-install-cover');
 	let backgroundHtml = document.getElementById('js-install-background');
 	let coverCheck = document.getElementById('js-preview__cover');
+	let coverField = document.getElementById('js-preview__cover-url');
+	let backgroundField = document.getElementById('js-preview__background-url');
 
 	if( theme.type === 'classic' ){
 		coverHtml.remove();
 		backgroundHtml.remove();
-		document.getElementById('js-preview-options__cover').remove();
+		coverField.parentNode.remove();
+		backgroundField.parentNode.remove();
+		coverCheck.parentNode.remove();
 	}
 	else {
 		let hasCustomInstall = false;
@@ -1302,18 +1306,25 @@ function pageSetup( ){
 			hasCustomInstall = true;
 
 			let choice = theme.cover === true ? 'Yes' : 'No';
-			let extra = '';
+			let extraConfig = '';
+			let extraInstall = '';
 
 			if( choice === 'Yes' ){
-				extra = `Be sure to upload an image by using the "Browse..." button.`;
+				extraConfig = `Be sure to upload an image by using the "Browse..." button.`;
+			}
+			if( 'cover_url' in theme ){
+				extraConfig += ` Use <a class="hyperlink" href="${theme.cover_url}" target="_blank">this image</a>.`;
+				extraInstall += ` This theme recommends <a class="hyperlink" href="${theme.cover_url}" target="_blank">this image</a>.`;
 			}
 			
-			customInstallTexts.push(`Set the "Show cover image" option to "<b>${choice}</b>".`);
+			customInstallTexts.push(`Set the "Show cover image" option to "<b>${choice}</b>".${extraConfig}`);
 			coverHtml.innerHTML = `
 				<p class="popup__paragraph">
-					In the sidebar, find the "Cover Image" area. Click to expand it if necessary. Set the "Show cover image" option to "<b>${choice}</b>". ${extra}
+					In the sidebar, find the "Cover Image" area. Click to expand it if necessary. Set the "Show cover image" option to "<b>${choice}</b>".${extraInstall}
 				</p>
 			`;
+
+			
 		}
 		else {
 			coverHtml.remove();
@@ -1322,16 +1333,22 @@ function pageSetup( ){
 		if( 'background' in theme ){
 			hasCustomInstall = true;
 
-			let choice = theme.background === true ? 'Yes' : 'No',
-				extra = '';
-			if( choice === 'Yes' ){
-				extra = `Be sure to upload an image by using the "Browse..." button.`;
-			}
+			let choice = theme.background === true ? 'Yes' : 'No';
+			let extraConfig = '';
+			let extraInstall = '';
 
-			customInstallTexts.push(`Set the "Show background image" option to "<b>${choice}</b>".`);
+			if( choice === 'Yes' ){
+				extraConfig = `Be sure to upload an image by using the "Browse..." button.`;
+			}
+			if( 'background_url' in theme ){
+				extraConfig += ` Use <a class="hyperlink" href="${theme.background_url}" target="_blank">this image</a>.`;
+				extraInstall += ` This theme recommends <a class="hyperlink" href="${theme.background_url}" target="_blank">this image</a>.`;
+			}
+			
+			customInstallTexts.push(`Set the "Show background image" option to "<b>${choice}</b>".${extraConfig}`);
 			backgroundHtml.innerHTML = `
 				<p class="popup__paragraph">
-					In the sidebar, find the "Background Image" area. Click to expand it if necessary. Set the "Show background image" option to "<b>${choice}</b>". ${extra}
+					In the sidebar, find the "Background Image" area. Click to expand it if necessary. Set the "Show background image" option to "<b>${choice}</b>".${extraInstall}
 				</p>
 			`;
 		}
@@ -1359,12 +1376,26 @@ function pageSetup( ){
 
 	// Cover
 	if( 'cover' in theme.preview ){
-		let val = theme.preview.cover;
 		// change toggle value unless it was already changed by regular settings
 		if( coverCheck.disabled === false ){
-			coverCheck.checked = val;
+			coverCheck.checked = theme.preview.cover;
 		}
-		postToPreview(['cover', val]);
+		postToPreview(['cover', theme.preview.cover]);
+	}
+	if( 'background' in theme.preview ){
+		postToPreview(['background', theme.preview.background]);
+	}
+	if( 'cover_url' in theme.preview ){
+		coverField.value = theme.preview.cover_url;
+		postToPreview(['cover_url', theme.preview.cover_url]);
+	}
+	if( 'background_url' in theme.preview ){
+		backgroundField.value = theme.preview.background_url;
+		postToPreview(['background_url', theme.preview.background_url]);
+	}
+	// disable background by default because most themes look better without it.
+	else if( theme.preview?.background !== true ) {
+		postToPreview(['background_url', false]);
 	}
 
 	// Category

.gitignore
@@ -4,3 +4,4 @@
 _site
 *Gemfile*
 *.psd
+.jekyll-metadata

assets/common.js
@@ -1373,21 +1373,27 @@ class Validate {
 	}
 
 	static display( display ){
-		if( 'cover' in display ){
-			if( this.permissive ){
-				display.cover = parseBool(display.cover);
-			}
-			if( !isBool(display.cover) ){
-				throw new Error(`"cover" value must be a boolean.`);
+		const boolean_keys = ['cover', 'background'];
+		for( let key of boolean_keys ){
+			if( key in display ){
+				if( this.permissive ){
+					display[key] = parseBool(display[key]);
+				}
+				if( !isBool(display[key]) ){
+					throw new Error(`"${key}" value must be a boolean.`);
+				}
 			}
 		}
-		
-		if( 'background' in display ){
-			if( this.permissive ){
-				display.background = parseBool(display.background);
-			}
-			if( !isBool(display.background) ){
-				throw new Error(`"background" value must be a boolean.`);
+
+		const url_keys = ['cover_url', 'background_url'];
+		for( let key of url_keys ){
+			if( key in display ){
+				if( !isString(display[key]) ){
+					throw new Error(`"${key}" value must be a string.`);
+				}
+				if( !isValidHttp(display[key]) ){
+					throw new Error(`"${key}" string values must be a valid URL of protocol "http".`);
+				}
 			}
 		}
 			

assets/customiser.css
@@ -198,6 +198,9 @@ input:checked + .toggle.is-forced::after {
 	align-items: center;
 	flex-shrink: 0;
 }
+.topbar__section--right {
+	margin-left: auto;
+}
 
 
 /* Options */
@@ -213,7 +216,9 @@ input:checked + .toggle.is-forced::after {
 	top: 50px;
 	right: 0;
 	display: flex;
-	padding: 15px;
+	flex-flow: column wrap;
+	align-items: start;
+	padding: 16px;
 	background: var(--background);
 	border: 2px solid var(--accent);
 	border-radius: 8px;
@@ -229,7 +234,7 @@ input:checked + .toggle.is-forced::after {
 }
 
 .options__item + .options__item {
-	margin-left: 15px;
+	margin-top: 8px;
 }
 
 .options__title {

assets/preview_common.js
@@ -39,6 +39,15 @@ window.addEventListener(
 			else if( type === 'cover' ){
 				toggleCover(content);
 			}
+			else if( type === 'background' ){
+				toggleBackground(content);
+			}
+			else if( type === 'cover_url' ){
+				setCover(content);
+			}
+			else if( type === 'background_url' ){
+				setBackground(content);
+			}
 			else if( type === 'style' ){
 				console.log(`This option is still to do. Style #${content}`);
 				//changeStyle(content);

assets/preview_modern.js
@@ -237,7 +237,7 @@ function setView( view ){
 			listMenu.innerHTML = listMenuInner;
 		}
 		
-		for( btn of itemBtns ){
+		for( let btn of itemBtns ){
 			btn.className = 'edit';
 			btn.firstChild.textContent = 'Edit';
 		}
@@ -316,6 +316,47 @@ function toggleCover( set = undefined ){
 	else {
 		cover.classList.toggle('hide');
 	}
+}
+
+function setCover( string = '' ){
+	if( string === '' ){
+		string = 'https://cdn.myanimelist.net/s/common/uploaded_files/1455540405-b2e2bf20e11b68631e8439b44d9a51c7.png';
+	}
+	document.querySelector('#cover-image').src = string;
+}
+
+var backgroundState = false;
+function toggleBackground( set = undefined ){
+	const style = document.querySelector('#default-css');
+	if( set === true ){
+		// set background to default
+		setBackground(true);
+	}
+	else if( set === false ){
+		setBackground(false);
+	}
+	else {
+		// invert current state
+		backgroundState = backgroundState === true ? false : true;
+		toggleBackground(backgroundState);
+	}
+}
+
+function setBackground( url = true ){
+	let ruleset = '';
+	if( url !== false ){
+		if( url === true || url === '' ){
+			url = 'https://cdn.myanimelist.net/s/common/uploaded_files/1455540188-934a8b8942494df1086f9402bbb5330b.png';
+		}
+		ruleset = `
+			background-image: url(${url});
+			background-attachment: fixed;
+			background-position: center top;
+			background-repeat: repeat\\2D x;
+		`;
+	}
+	const style = document.querySelector('#default-css');
+	style.textContent = style.textContent.replace(/(body\.ownlist\s*\{)[^}]*\}/, `$1${ruleset}}`);
 }
 
 let stats = document.getElementsByClassName('list-stats')[0];

preview/modern/animelist.html
@@ -3,8 +3,13 @@
 <html>
 
 <head>
-  <style type="text/css">
-    body.ownlist {}
+  <style id="default-css" type="text/css">
+    body.ownlist {
+          background-image: url("https://cdn.myanimelist.net/s/common/uploaded_files/1455540188-934a8b8942494df1086f9402bbb5330b.png");
+              background-attachment: fixed;
+              background-position: center top;
+              background-repeat: repeat\2D x;
+        }
 
     .list-unit .list-stats a {
   color: unset;

theme.html
@@ -53,7 +53,7 @@ permalink: /theme
 						</a>
 					</nav>
 				</div>
-				<div class="topbar__section">
+				<div class="topbar__section topbar__section--right">
 					<div class="discrepancy-notice">
 						<div class="discrepancy-notice__title">
 							Preview will differ from the final outcome.
@@ -72,10 +72,20 @@ permalink: /theme
 						</a>
 
 						<div id="js-preview-options-container" class="options__container is-hidden">
-							<div class="options__item" id="js-preview-options__cover">
+							<div class="options__item">
 								<b class="options__title">Cover Image</b>
 								<input id="js-preview__cover" type="checkbox" class="o-hidden" checked="checked">
 								<label class="toggle" for="js-preview__cover" onclick="postToPreview(['cover', !document.getElementById('js-preview__cover').checked])" data-info="This option is not editable due to the recommended theme settings."></label>
+							</div>
+
+							<div class="options__item">
+								<b class="options__title">Cover URL</b>
+								<input id="js-preview__cover-url" type="text" class="input" placeholder="https://example.com/yourimage.jpg" oninput="postToPreview(['cover_url', this.value])">
+							</div>
+
+							<div class="options__item">
+								<b class="options__title">Background URL</b>
+								<input id="js-preview__background-url" type="text" class="input" placeholder="https://example.com/yourimage.jpg" oninput="postToPreview(['background_url', this.value])">
 							</div>
 
 							<div class="options__item">

commit d34b628ce313868ac2df0236cc60d864b37ab03c

Fix console error on classic lists
Error introduced in 250bea24539c1cce1eff61c68f689a7aa5675aaa

assets/customiser.js
@@ -1397,7 +1397,7 @@ function pageSetup( ){
 		postToPreview(['background_url', theme.preview.background_url]);
 	}
 	// disable background by default because most themes look better without it.
-	else if( theme.preview?.background !== true ) {
+	else if( theme.preview?.background !== true && theme.type === 'modern' ) {
 		postToPreview(['background_url', false]);
 	}
 